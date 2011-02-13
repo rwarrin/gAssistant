7cdee47c33a46a4d5be7334e5190753375196184
Rick Warrington <phial1020@gmail.com>
2011-02-13 14:34:31 -0700

    Added a help command with a list of all the slash commands.  And they're in color!

--- core.lua
+++ core.lua
@@ -3,12 +3,25 @@
 
 -- Ready Check
 -- Quickly initiate a ready check with a slash command
-
 SLASH_GREADYCHECK1 = "/rc";
 SlashCmdList["GREADYCHECK"] = function() DoReadyCheck(); end
 
+-- Convert To Party
+-- Effortlessly convert a Raid group of 5 or few players to a party
 SLASH_CONVERTTOPARTY1 = "/ctp";
 SlashCmdList["CONVERTTOPARTY"] = function() ConvertToParty(); end
 
+-- Convert To Raid
+-- Quickly convert a party to a raid group.
 SLASH_CONVERTTORAID1 = "/ctr";
 SlashCmdList["CONVERTTORAID"] = function() ConvertToRaid(); end
+
+-- Help
+-- Display a list of the available slash commands and their functions
+SLASH_GASSISTHELP1 = "/gahelp";
+SlashCmdList["GASSISTHELP"] = function()
+	print("|cff7bb333gAssistant available slash commands:|r");
+	print("|cff7bb333gAssistant:|r  |cffb33333/rc|r  - |cff33b1b3Ready Check.|r");
+	print("|cff7bb333gAssistant:|r  |cffb33333/ctp|r - |cff33b1b3Convert a raid group to a party.|r");
+	print("|cff7bb333gAssistant:|r  |cffb33333/ctr|r - |cff33b1b3Convert a party to a raid group.|r");
+end
